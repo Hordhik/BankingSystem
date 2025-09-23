e7cc78714e7d70dc0623719f9b8f11b93dd4f40f
“Hordhik” <kondamanikant4@gmail.com>
2025-09-24 00:19:27 +0530

    Finished Header

---
+++ src/Dashboard/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+        <div className="name">
+            <div className="logo"></div>
+            <div className="id">hordhik.reddy@ybl</div>
+        </div>
+        <div className="menu">
+            <div className="menu-option active">Accounts</div>
+            <div className="menu-option">Credit Card</div>
+            <div className="menu-option">Transfer Funds</div>
+            <div className="menu-option">Loans</div>
+            <div className="menu-option">Offers</div>
+            </div>
+        <div className="logout">
+            <div className="menu-option">Raise disputes</div>
+            <div className="menu-option logout">Log Out</div>
+        </div>
+    </div>
+  )
+}
