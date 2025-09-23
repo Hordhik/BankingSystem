31ba34c4740c0d87021d5d46c03a6974b3dedbe2
“Hordhik” <kondamanikant4@gmail.com>
2025-09-24 00:19:27 +0530

    Finished Header

---
+++ src/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Dashboard.css'
+import { Header } from '../Dashboard/Header/Header'
+
+
+export const Dashboard = () => {
+  return (
+    <div className='dashboard'>
+        <Header></Header>
+    </div>
+  )
+}
