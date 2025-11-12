882f691e789361a1bc960f5b8eed353dbdc7e482
VENKAT2905 <venkatchitrakvi@gmail.com>
2025-11-13 04:21:13 +0530

    deleted backup files

--- Server/backup_bom_20251113_022031/UserRepository.java
+++
@@ -1,8 +0,0 @@
-﻿package com.bankingapp.Server.repository;
-import com.bankingapp.Server.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-    Optional<User> findByEmail(String email);
-}
